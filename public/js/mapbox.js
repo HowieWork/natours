34750c1127dd1200cefeda9c90f930f9610c8d55
Howie <howiewang2020@gmail.com>
2021-09-15 07:30:36 -0400

    Update section12

---
+++ public/js/mapbox.js
@@ -0,0 +1,47 @@
+/* eslint-disable */
+const locations = JSON.parse(document.getElementById('map').dataset.locations);
+console.log(locations);
+mapboxgl.accessToken =
+  'pk.eyJ1IjoiaG93aWV3b3JrIiwiYSI6ImNrdGoxeDVpODE3aHIydW1wZHF5ODJyMGoifQ.11L7FeMU0LwqfepiXh2A4g';
+var map = new mapboxgl.Map({
+  container: 'map',
+  style: 'mapbox://styles/howiework/cktj5j2cn402717uiffxiya3l',
+  scrollZoom: false,
+  // center: [-118.319569, 34.079207],
+  // zoom: 10,
+  // interactive: false,
+});
+
+const bounds = new mapboxgl.LngLatBounds();
+
+locations.forEach((loc) => {
+  // Create marker
+  const el = document.createElement('div');
+  el.className = 'marker';
+
+  // Add marker
+  new mapboxgl.Marker({
+    element: el,
+    anchor: 'bottom',
+  })
+    .setLngLat(loc.coordinates)
+    .addTo(map);
+
+  // Add popup
+  new mapboxgl.Popup({ offset: 30 })
+    .setLngLat(loc.coordinates)
+    .setHTML(`<p>Day ${loc.day}: ${loc.description}</p>`)
+    .addTo(map);
+
+  // Extend map bounds to include current location
+  bounds.extend(loc.coordinates);
+});
+
+map.fitBounds(bounds, {
+  padding: {
+    top: 200,
+    bottom: 150,
+    left: 100,
+    right: 100,
+  },
+});
