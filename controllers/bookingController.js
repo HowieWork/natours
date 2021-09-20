4e2bd8585de3966b69b6f04bbedde7c77db3c16f
Howie <howiewang2020@gmail.com>
2021-09-20 15:22:26 -0400

    Update bookingController.js

--- controllers/bookingController.js
+++ controllers/bookingController.js
@@ -55,9 +55,10 @@ exports.createBookingCheckout = catchAsync(async (req, res, next) => {
 
 const createBookingCheckout = async (session) => {
   const tour = session.client_reference_id;
-  const user = (await User.findOne({ email: session.customer_email })).id;
-  const price = session.display_items[0].amount / 100;
-  console.log(tour, user, price);
+  const user = (await User.findOne({ email: session.customer_details.email }))
+    .id;
+  const price = session.amount_total / 100;
+
   await Booking.create({ tour, user, price });
 };
 
